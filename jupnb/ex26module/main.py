1e5e8f0aa1a0731668e4e83f01569b0ecef781bf
AndreyArhip <pavlov_948@mail.ru>
2020-05-05 20:23:46 +0700

    2 ex module package in py

---
+++ jupnb/ex26module/main.py
@@ -0,0 +1,8 @@
+# from my_package import my_module
+from my_package.my_module import summ
+
+a=10
+b=24
+print('{} + {} = {}'.format(a,b,summ(a,b)))
+# print('{} + {} = {}'.format(a,b,my_module.summ(a,b)))
+# print('{} - {} = {}'.format(a,b,my_module.diff(a,b)))
